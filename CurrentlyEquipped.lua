d37b709749eddbdae6335eeea684ac6d9ad4535b
AlekWithK <38448418+AlekWithK@users.noreply.github.com>
2022-12-15 03:45:32 -0800

    UI framework added

--- CurrentlyEquipped.lua
+++ CurrentlyEquipped.lua
@@ -4,15 +4,42 @@ CE.name = "CurrentlyEquipped"
 CE.author = "|c1cf9e4A|r|c39f3cal|r|c55eeb1e|r|c71e897k|r|c8ee27dW|r|caadc63i|r|cc6d74at|r|ce3d130h|r|cffcb16K|r"
 CE.version = "1.0.0"
 
-local defaults = {}
+--Move these?
+CE.unlockedUI = true
+CE.x_offset = 1000
+CE.y_offset = 500
 
 --Addon initialize
 function CE.Init()
-    d("CE Loaded!")
+    CE.AddonMenu()
+
+    CE.savedVariables = ZO_SavedVars:NewAccountWide("CurrentlyEquippedSavedVariables", 1, nil, {})
+    
+    --DEBUGGING
+    --EVENT_MANAGER:RegisterForEvent(CE.name, EVENT_RETICLE_TARGET_CHANGED, CE.SavePos)
+    CE.RestorePos()
 end
 
---function CE.registerEvents()
---function CE.loadDefaults()
+--Restore UI position
+function CE.RestorePos()
+    local left = CE.savedVariables.left or CE.x_offset
+    local top = CE.savedVariables.top or CE.y_offset
+
+    CEFrame:ClearAnchors()
+    CEFrame:SetAnchor(TOPLEFT, GuiRoot, TOPLEFT, left, top)
+end
+
+--Move UI
+function CE.MoveUI()
+    if CE.unlockedUI then CEFrame:SetHidden(false)
+    else CEFrame:SetHidden(true) end
+end
+
+--Save variables
+function CE.SavePos()
+    CE.savedVariables.left = CEFrame:GetLeft()
+    CE.savedVariables.top = CEFrame:GetTop()
+end
 
 --Addon startup
 function CE.OnAddOnLoaded(_, addonName)
--- CurrentlyEquippedMenu.lua
+++ CurrentlyEquippedMenu.lua
@@ -1,2 +1,42 @@
 CurrentlyEquipped = CurrentlyEquipped or {}
 local CE = CurrentlyEquipped
+
+function CE.AddonMenu()
+
+    local menuOptions = {
+        type = "panel",
+        name = CE.name,
+        displayName = "Currently Equipped Gear",
+        author = CE.author,
+        version = CE.version,
+        slashCommand = "/ce",
+        registerForRefresh = true,
+        registerForDefaults = true,
+    }
+
+    local dataTable = {
+        {
+            type = "description",
+            text = "Displays your currently equipped sets, with colors indicating their status."
+        },
+        {
+            type = "header",
+            name = "Settings",
+            width = "full",
+        },
+        {
+			type    = "checkbox",
+			name    = "Unlock UI",
+			tooltip = "Toggle 'On' to move display's on screen position",
+			default = false,
+			getFunc = function() return false end,
+			setFunc = function(newVal) CE.MoveUI() CE.unlockedUI = not newVal; end,
+		},
+
+
+    }
+
+    LAM = LibAddonMenu2
+	LAM:RegisterAddonPanel(CE.name .. "Options", menuOptions)
+	LAM:RegisterOptionControls(CE.name .. "Options", dataTable)
+end
